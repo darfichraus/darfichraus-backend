a4b10e11876d3d7ca1fdee3d2474473c61008220
BSchneppe <benedikt.schneppe@gmail.com>
2020-03-22 11:39:02 +0100

    fix build

--- src/main/java/de/darfichraus/service/MappingService.java
+++ src/main/java/de/darfichraus/service/MappingService.java
@@ -1,7 +1,7 @@
 package de.darfichraus.service;
 
-import de.darfichraus.entity.Areal;
 import de.darfichraus.entity.Mapping;
+import de.darfichraus.entity.enums.Areal;
 import de.darfichraus.repository.MappingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
