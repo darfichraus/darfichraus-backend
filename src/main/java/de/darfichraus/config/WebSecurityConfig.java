fb8d6c6710a3893cca93aa676690460751eaae2c
BSchneppe <benedikt.schneppe@gmail.com>
2020-03-24 18:17:51 +0100

    change api key auth to work with cors requests

--- src/main/java/de/darfichraus/config/WebSecurityConfig.java
+++ src/main/java/de/darfichraus/config/WebSecurityConfig.java
@@ -1,26 +1,14 @@
 package de.darfichraus.config;
 
-
-import de.darfichraus.filters.CorsPreflightBypassFilter;
+import de.darfichraus.filters.APIKeyAuthFilter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,43 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        RequestHeaderAuthenticationFilter filter = new RequestHeaderAuthenticationFilter();
-        filter.setPrincipalRequestHeader(authHeaderName);
-        filter.setAuthenticationManager(authentication -> {
-            String principal = (String) authentication.getPrincipal();
-            if (!authHeaderValue.equals(principal)) {
-                throw new BadCredentialsException("The API key was not valid.");
-            }
-            authentication.setAuthenticated(true);
-            return authentication;
-        });
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(authHeaderName, authHeaderValue);
 
-        httpSecurity.cors().and()
+        httpSecurity.
+                antMatcher("/**")
                 .csrf()
                 .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(new CorsPreflightBypassFilter(), filter.getClass())
-                .addFilterAfter(filter, CorsPreflightBypassFilter.class)
-                .addFilterBefore(new ExceptionTranslationFilter(
-                                new Http403ForbiddenEntryPoint()),
-                        filter.getClass()
-                )
-                .authorizeRequests()
-                .anyRequest().authenticated();
+                .addFilterAfter(filter, BasicAuthenticationFilter.class);
 
-
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
 }
