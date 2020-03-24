b00f644e0c7e3d511163cb7231cfc3adc8a642eb
BSchneppe <benedikt.schneppe@gmail.com>
2020-03-24 18:06:38 +0100

    try to fix cors issues

--- src/main/java/de/darfichraus/filters/CorsPreflightBypassFilter.java
+++ src/main/java/de/darfichraus/filters/CorsPreflightBypassFilter.java
@@ -17,6 +17,11 @@ public class CorsPreflightBypassFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers", "*");
         if (httpServletRequest.getMethod().equals(HttpMethod.OPTIONS.name())) {
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
         } else {
