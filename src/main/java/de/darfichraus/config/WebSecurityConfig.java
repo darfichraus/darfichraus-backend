b728a66ab3899dc30d7f55f9308cc9e65924149f
BSchneppe <benedikt.schneppe@gmail.com>
2020-03-29 18:24:47 +0200

    fix cors ? :fearful:

--- src/main/java/de/darfichraus/config/WebSecurityConfig.java
+++ src/main/java/de/darfichraus/config/WebSecurityConfig.java
@@ -1,8 +1,9 @@
 package de.darfichraus.config;
 
 
-import de.darfichraus.filters.CorsPreflightBypassFilter;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
@@ -14,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -42,12 +47,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         httpSecurity
                 .csrf()
-                .disable()
+                .disable().cors().and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(new CorsPreflightBypassFilter(), filter.getClass())
-                .addFilterAfter(filter, CorsPreflightBypassFilter.class)
+                .addFilterAfter(filter, BasicAuthenticationFilter.class)
                 .addFilterBefore(new ExceptionTranslationFilter(
                                 new Http403ForbiddenEntryPoint()),
                         filter.getClass()
@@ -56,6 +60,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("API-KEY", "Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
