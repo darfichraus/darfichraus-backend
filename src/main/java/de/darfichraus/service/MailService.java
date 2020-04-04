e1c07d1b13e1b81032a7e044160bcc32e431360e
BSchneppe <benedikt.schneppe@gmail.com>
2020-04-04 12:01:09 +0200

    make MailService a spring Bean :man_facepalming:

--- src/main/java/de/darfichraus/service/MailService.java
+++ src/main/java/de/darfichraus/service/MailService.java
@@ -3,9 +3,11 @@ package de.darfichraus.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 
+@Service
 public class MailService {
 
     @Autowired
